869a8663446c73ade83bfe7ea209c917614299b7
gouravnandhu <126135655+gouravnandhu@users.noreply.github.com>
2024-02-25 14:25:26 +0530

    Add files via upload

---
+++ quest.py
@@ -0,0 +1,28 @@
+quiz_data = [
+    {
+        "question": "What will happen if in a C program you assign a value to an array element whose subscript exceeds the size of array?",
+        "choices": ["The element will be set to 0.", "The compiler would report an error.", "The program may crash if some important data gets overwritten.", "The array size would appropriately grow."],
+        "answer": "The program may crash if some important data gets overwritten."
+    },
+    {
+        "question": "Entomology is the science that studies.?",
+        "choices": ["Behavior of human beings", "Insects", "The origin and history of technical and scientific terms", "The formation of rocks"],
+        "answer": "Insects"
+    },
+    {
+        "question": "What is the chemical symbol for gold?",
+        "choices": ["Go", "Au", "Ag", "Gd"],
+        "answer": "Au"
+    },
+    {
+        "question": "Which country is known as the 'Land of the Rising Sun'?",
+        "choices": ["China", "Japan", "South Korea", "Thailand"],
+        "answer": "Japan"
+    },
+    {
+        "question": "Who is the first prime minister of India",
+        "choices": ["Narendra Modi", "Indira Ghandi", "Manmohan Sign", "Jawaharlal Nehru"],
+        "answer": "Jawaharlal Nehru"
+    }
+    # Add more questions here
+]
